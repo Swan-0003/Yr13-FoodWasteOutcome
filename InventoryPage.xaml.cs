76d96de484654daa0bf2352b94519896f89de7e1
Sidney <22009@my.westlakegirls.school.nz>
2026-08-17 10:50:15 +1200

    Added basic inventory functionality

--- InventoryPage.xaml.cs
+++ InventoryPage.xaml.cs
@@ -6,4 +6,43 @@ public partial class InventoryPage : ContentPage
     {
         InitializeComponent();
     }
+
+private void OnAddItemClicked(object sender, EventArgs e)
+{
+    string item = InventoryItemEntry.Text;
+
+    if (!string.IsNullOrWhiteSpace(item))
+    {
+        HorizontalStackLayout itemRow = new HorizontalStackLayout
+        {
+            Spacing = 10
+        };
+
+        Label newItem = new Label
+        {
+            Text = item,
+            FontSize = 16,
+            VerticalOptions = LayoutOptions.Center
+        };
+
+        Button deleteButton = new Button
+        {
+            Text = "Delete"
+        };
+
+        deleteButton.Clicked += (sender, e) =>
+        {
+            InventoryList.Children.Remove(itemRow);
+        };
+
+        itemRow.Children.Add(newItem);
+        itemRow.Children.Add(deleteButton);
+
+        InventoryList.Children.Add(itemRow);
+
+        InventoryItemEntry.Text = "";
+    }
+}
+
+
 }
