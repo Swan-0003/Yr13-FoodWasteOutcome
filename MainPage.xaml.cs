dd2f7585a488d31427553b53b4b8217689cc051f
Sidney <22009@my.westlakegirls.school.nz>
2026-08-23 23:17:58 +1200

    Connect Home expiry dashboard to inventory

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,45 @@
 
 public partial class MainPage : ContentPage
 {
+
+    private void UpdateUpcomingExpiry()
+{
+    FoodItem? upcomingFood = null;
+    int smallestDaysLeft = 4;
+
+    foreach (FoodItem food in FoodData.Items)
+    {
+        int daysLeft = (food.ExpiryDate.Date - DateTime.Today).Days;
+
+        if (daysLeft >= 0 && daysLeft <= 3 && daysLeft < smallestDaysLeft)
+        {
+            upcomingFood = food;
+            smallestDaysLeft = daysLeft;
+        }
+    }
+
+    if (upcomingFood == null)
+    {
+        UpcomingExpiryLabel.Text = "No food expiring soon";
+    }
+    else if (smallestDaysLeft == 0)
+    {
+        UpcomingExpiryLabel.Text = upcomingFood.Name + " expires today";
+    }
+    else if (smallestDaysLeft == 1)
+    {
+        UpcomingExpiryLabel.Text = upcomingFood.Name + " expires tomorrow";
+    }
+    else
+    {
+        UpcomingExpiryLabel.Text = upcomingFood.Name + " expires in " + smallestDaysLeft + " days";
+    }
+}
+protected override void OnAppearing()
+{
+    base.OnAppearing();
+    UpdateUpcomingExpiry();
+}
     public MainPage()
     {
         InitializeComponent();
